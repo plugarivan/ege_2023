ddc410d9662c10a94affe3fc42a7f3de14b6c0ff
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-12-10 12:44:48 +0300

    10-12

--- trash.py
+++ trash.py
@@ -1,10 +1,44 @@
-p = set(range(1, 43))
-q = set(range(25, 99))
-a = set()
-for x in range(-1000, 1000):
-    if not((x in q) <= (not(x in p) and (x in q) <= (x in a))):
-        a.add(x)
-print(a)
-print(max(a) - min(a))
+#19
+def f(x, y, p):
+    if x + y <= 30 and p == 2:
+        return True
+    else:
+        if x + y > 30 and p == 2:
+            return False
+    return f(x - 1, y, p + 1) or f(x // 2 + 1, y, p + 1) or \
+        f(x, y - 1, p + 1) or f(x, y // 2 + 1, p + 1)
 
 
+print([i for i in range(100, 1, -1) if f(18, i, 0)])
+#20
+def f(x, y, p):
+    if x + y <= 30 or p > 2:
+        return p == 2
+    if p % 2:
+        return f(x - 1, y, p + 1) and f(x // 2 + 1, y, p + 1) and \
+               f(x, y - 1, p + 1) and f(x, y // 2 + 1, p + 1)
+    else:
+        return f(x - 1, y, p + 1) or f(x // 2 + 1, y, p + 1) or \
+        f(x, y - 1, p + 1) or f(x, y // 2 + 1, p + 1)
+
+
+print(sorted([i for i in range(100, 1, -1) if f(18, i, 0)]))
+#21
+def f(x, y, p):
+    if x + y <= 30 and (p == 2 or p == 4):
+        return True
+    else:
+        if x + y > 30 and p == 4:
+            return False
+        else:
+            if x + y <= 30:
+                return False
+    if p % 2 == 0:
+        return f(x - 1, y, p + 1) and f(x / 2 + 1, y, p + 1) and \
+               f(x, y - 1, p + 1) and f(x, y / 2 + 1, p + 1)
+    else:
+        return f(x - 1, y, p + 1) or f(x / 2 + 1, y, p + 1) or \
+        f(x, y - 1, p + 1) or f(x, y / 2 +1, p + 1)
+
+
+print(max([i for i in range(100, 1, -1) if f(18, i, 0)]))
