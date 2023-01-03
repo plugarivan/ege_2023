98d4e768c84b4c63c26a6535032c34e69f0b207f
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2023-01-03 11:29:07 +0300

    03-01

---
+++ task24/24-6.py
@@ -0,0 +1,14 @@
+'''
+(А.М. Кабанов) В текстовом файле k7a-6.txt находится цепочка из символов латинского алфавита A, B, C, D, E, F.
+Найдите длину самой длинной подцепочки, не содержащей гласных букв.
+'''
+with open('../files/task24/k7a-6.txt') as f:
+    s = f.readline()
+    k, kmax = 0, 0
+    for i in s:
+        if i not in 'AE':
+            k += 1
+            kmax = max(k, kmax)
+        else:
+            k = 0
+print(kmax)
