00a8fb6520f3c7ca94fa5b4dd9b2137cbc646055
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2023-01-30 09:07:09 +0300

    30-01

--- trash.py
+++ trash.py
@@ -1,1 +1,15 @@
-print(f'{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60))
+def rot13(message):
+    res = ''
+    for item in message:
+        if (ord(item)>= ord('A') and ord(item)<= ord('M')) or (ord(item)>= ord('a') and ord(item)<= ord('m')):
+            res += chr(ord(item)+13)
+        elif (ord(item)>= ord('N') and ord(item)<= ord('Z')) or (ord(item)>= ord('n') and ord(item)<= ord('z')):
+            res += chr(ord(item)-13)
+        else:
+            res += item
+    return res
+
+
+
+
+
