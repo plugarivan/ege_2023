6c58c328f9141dc49f513271cc55df19f495e123
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2023-03-18 16:46:09 +0300

    18-03

--- trash.py
+++ trash.py
@@ -1,5 +1,33 @@
-for n in range(1263050, 129963960):
-    s = str(n)
-    if s[:2] == '12' and s[-5:-3] == '63' and s[-2] == '5' and n % 3123 == 0:
-        print(n)
+#19 задание
+def f(x, y, p):
+    if x + y >= 479 or p > 2:
+        return p == 2
+    return f(x + 1, y, p + 1) or f(x + 3, y, p + 1) or f(x * 2, y, p + 1) or f(x, y + 1, p + 1) or f(x, y + 3, p + 1) or f(x, y * 2, p + 1)
 
+
+print([i for i in range(1, 478) if f(239, i, 0)])
+#20 задание
+def f(x, y, p):
+    if x + y >= 479 or p > 3:
+        return p == 3
+    if p % 2:
+        return f(x + 1, y, p + 1) and f(x + 3, y, p + 1) and f(x * 2, y, p + 1) and f(x, y + 1, p + 1) and f(x, y + 3, p + 1) and f(x, y * 2, p + 1)
+    else:
+        return f(x + 1, y, p + 1) or f(x + 3, y, p + 1) or f(x * 2, y, p + 1) or f(x, y + 1, p + 1) or f(x, y + 3, p + 1) or f(x, y * 2, p + 1)
+
+
+print([i for i in range(1, 478) if f(239, i, 0)])
+
+#21 задание
+def f(x, y, p):
+    if x + y >= 479 or p > 4:
+        return p == 2 or p == 4
+    if p % 2 == 0:
+        return f(x + 1, y, p + 1) and f(x * 2, y, p + 1) and f(x, y + 1, p + 1) \
+            and f(x, y * 2, p + 1)
+    else:
+        return f(x + 1, y, p + 1) or f(x * 2, y, p + 1) or f(x, y + 1, p + 1) \
+or f(x, y * 2, p + 1)
+
+
+print(min([i for i in range(1, 478) if f(239, i, 0)]))
